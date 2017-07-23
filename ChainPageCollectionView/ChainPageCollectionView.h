48e2b42161a4286b12824b2d3934ad34f8990a6d
jindulys <liyansong.edw@gmail.com>
2017-07-22 23:33:02 -0400

    Initial Commit

---
+++ ChainPageCollectionView/ChainPageCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  ChainPageCollectionView.h
+//  ChainPageCollectionView
+//
+//  Created by yansong li on 2017-07-22.
+//  Copyright © 2017 yansong li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChainPageCollectionView.
+FOUNDATION_EXPORT double ChainPageCollectionViewVersionNumber;
+
+//! Project version string for ChainPageCollectionView.
+FOUNDATION_EXPORT const unsigned char ChainPageCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChainPageCollectionView/PublicHeader.h>
+
+
