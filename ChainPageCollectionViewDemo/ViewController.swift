c29f9581a4994838fa33fe4913c251dc90473042
jindulys <liyansong.edw@gmail.com>
2017-07-23 12:47:15 -0400

    Update Demo.

--- ChainPageCollectionViewDemo/ViewController.swift
+++ ChainPageCollectionViewDemo/ViewController.swift
@@ -14,17 +14,17 @@ class ViewController: UIViewController {
   let parentDataSource = ["Yelp", "Facebook", "Netflix", "Yahoo", "Google", "Indeed", "Ritual",
                           "Sina", "New York Times", "Firefox", "Gilt"]
   
-  let childDataSouce = [["Yelp has a big news 1", "Yelp has a big news 2", "Yelp has a big news 3", "Yelp has a big news 4", "Yelp has a big news 5", "Yelp has a big news 6"],
-                        ["Facebook has a big news 1", "Facebook has a big news 2", "Facebook has a big news 3", "Facebook has a big news 4", "Facebook has a big news 5", "YeFacebooklp has a big news 6"],
-                        ["Netflix has a big news 1", "Netflix has a big news 2", "Netflix has a big news 3", "Netflix has a big news 4", "Netflix has a big news 5", "Netflix has a big news 6"],
-                        ["Yahoo has a big news 1", "Yahoo has a big news 2", "Yahoo has a big news 3", "Yahoo has a big news 4", "Yahoo has a big news 5", "Yahoo has a big news 6"],
-                        ["Google has a big news 1", "Google has a big news 2", "Google has a big news 3", "Google has a big news 4", "Google has a big news 5", "Google has a big news 6"],
-                        ["Indeed has a big news 1", "Indeed has a big news 2", "Indeed has a big news 3", "Indeed has a big news 4", "Indeed has a big news 5", "Indeed has a big news 6"],
-                        ["Ritual has a big news 1", "Ritual has a big news 2", "Ritual has a big news 3", "Ritual has a big news 4", "Ritual has a big news 5", "Ritual has a big news 6"],
-                        ["Sina has a big news 1", "Sina has a big news 2", "Sina has a big news 3", "Sina has a big news 4", "Sina has a big news 5", "Sina has a big news 6"],
-                        ["New York Times has a big news 1", "New York Times has a big news 2", "New York Times has a big news 3", "New York Times has a big news 4", "New York Times has a big news 5", "New York Times has a big news 6"],
-                        ["Firefox has a big news 1", "Firefox has a big news 2", "Firefox has a big news 3", "Firefox has a big news 4", "Firefox has a big news 5", "Firefox has a big news 6"],
-                        ["Gilt has a big news 1", "Gilt has a big news 2", "Gilt has a big news 3", "Gilt has a big news 4", "Gilt has a big news 5", "Gilt has a big news 6"]]
+  let childDataSouce = [["Yelp has some news 1", "Yelp has some news 2", "Yelp has some news 3", "Yelp has some news 4", "Yelp has some news 5", "Yelp has some news 6"],
+                        ["Facebook has some news 1", "Facebook has some news 2", "Facebook has some news 3", "Facebook has some news 4", "Facebook has some news 5", "YeFacebooklp has some news 6"],
+                        ["Netflix has some news 1", "Netflix has some news 2", "Netflix has some news 3", "Netflix has some news 4", "Netflix has some news 5", "Netflix has some news 6"],
+                        ["Yahoo has some news 1", "Yahoo has some news 2", "Yahoo has some news 3", "Yahoo has some news 4", "Yahoo has some news 5", "Yahoo has some news 6"],
+                        ["Google has some news 1", "Google has some news 2", "Google has some news 3", "Google has some news 4", "Google has some news 5", "Google has some news 6"],
+                        ["Indeed has some news 1", "Indeed has some news 2", "Indeed has some news 3", "Indeed has some news 4", "Indeed has some news 5", "Indeed has some news 6"],
+                        ["Ritual has some news 1", "Ritual has some news 2", "Ritual has some news 3", "Ritual has some news 4", "Ritual has some news 5", "Ritual has some news 6"],
+                        ["Sina has some news 1", "Sina has some news 2", "Sina has some news 3", "Sina has some news 4", "Sina has some news 5", "Sina has some news 6"],
+                        ["New York Times has some news 1", "New York Times has some news 2", "New York Times has some news 3", "New York Times has some news 4", "New York Times has some news 5", "New York Times has some news 6"],
+                        ["Firefox has some news 1", "Firefox has some news 2", "Firefox has some news 3", "Firefox has some news 4", "Firefox has some news 5", "Firefox has some news 6"],
+                        ["Gilt has some news 1", "Gilt has some news 2", "Gilt has some news 3", "Gilt has some news 4", "Gilt has some news 5", "Gilt has some news 6"]]
   
   var parentIndex: Int = 0
   
