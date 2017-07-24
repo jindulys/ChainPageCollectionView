22bf9118e5158728ab31023ff32b3f8fd4699112
jindulys <liyansong.edw@gmail.com>
2017-07-24 07:51:51 -0400

    Add in Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  ChainPageCollectionView
+//
+//  Created by yansong li on 2017-07-24.
+//  Copyright © 2017 yansong li. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "ChainPageCollectionView"
+)
