ff8099d23f6fbe599fa354e03ff179375eb41f6e
jindulys <liyansong.edw@gmail.com>
2017-07-24 20:48:18 -0400

    Update Demo.

--- ChainPageCollectionViewDemo/ImageCardCollectionCell.swift
+++ ChainPageCollectionViewDemo/ImageCardCollectionCell.swift
@@ -11,26 +11,20 @@ import UIKit
 /// The base card collection cell.
 public class ImageCardCollectionViewCell: UICollectionViewCell {
   
-  public var textLabel: UILabel!
-  
   public var backGroundImageView: UIImageView!
   
   public override init(frame: CGRect) {
     super.init(frame: frame)
-    self.layer.cornerRadius = 6.0
-    self.clipsToBounds = true
-    textLabel = UILabel()
-    textLabel.numberOfLines = 0
-    textLabel.clipsToBounds = true
-    textLabel.translatesAutoresizingMaskIntoConstraints = false
-    textLabel.textAlignment = .center
-    textLabel.textColor = UIColor.white
-    textLabel.font = UIFont.systemFont(ofSize: 30.0)
+    self.layer.shadowColor = UIColor.black.cgColor
+    self.layer.shadowOffset = CGSize(width: 1, height: 1)
+    self.layer.shadowOpacity = 0.7
+    self.layer.shadowRadius = 4.0
     backGroundImageView = UIImageView()
     backGroundImageView.contentMode = .scaleAspectFill
     backGroundImageView.translatesAutoresizingMaskIntoConstraints = false
-    self.addSubview(backGroundImageView)
-    self.addSubview(textLabel)
+    self.contentView.layer.masksToBounds = true
+    self.contentView.layer.cornerRadius = 8.0
+    self.contentView.addSubview(backGroundImageView)
     buildConstraint()
   }
   
@@ -38,15 +32,12 @@ public class ImageCardCollectionViewCell: UICollectionViewCell {
     fatalError("init(coder:) has not been implemented")
   }
   
+  // MARK: Private
   private func buildConstraint() {
-    textLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-    textLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-    textLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-    textLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-    backGroundImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-    backGroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-    backGroundImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-    backGroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    self.backGroundImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor).isActive = true
+    self.backGroundImageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
+    self.backGroundImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+    self.backGroundImageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
   }
 }
 
--- ChainPageCollectionViewDemo/ViewController.swift
+++ ChainPageCollectionViewDemo/ViewController.swift
@@ -12,19 +12,7 @@ import ChainPageCollectionView
 class ViewController: UIViewController {
   
   let parentDataSource = ["yelp", "facebook", "netflix", "yahoo", "google", "indeed", "ritual",
-                          "sina", "new york time", "firefox", "gilt"]
-  
-  let childDataSouce = [["Yelp has some news 1", "Yelp has some news 2", "Yelp has some news 3", "Yelp has some news 4", "Yelp has some news 5", "Yelp has some news 6"],
-                        ["Facebook has some news 1", "Facebook has some news 2", "Facebook has some news 3", "Facebook has some news 4", "Facebook has some news 5", "YeFacebooklp has some news 6"],
-                        ["Netflix has some news 1", "Netflix has some news 2", "Netflix has some news 3", "Netflix has some news 4", "Netflix has some news 5", "Netflix has some news 6"],
-                        ["Yahoo has some news 1", "Yahoo has some news 2", "Yahoo has some news 3", "Yahoo has some news 4", "Yahoo has some news 5", "Yahoo has some news 6"],
-                        ["Google has some news 1", "Google has some news 2", "Google has some news 3", "Google has some news 4", "Google has some news 5", "Google has some news 6"],
-                        ["Indeed has some news 1", "Indeed has some news 2", "Indeed has some news 3", "Indeed has some news 4", "Indeed has some news 5", "Indeed has some news 6"],
-                        ["Ritual has some news 1", "Ritual has some news 2", "Ritual has some news 3", "Ritual has some news 4", "Ritual has some news 5", "Ritual has some news 6"],
-                        ["Sina has some news 1", "Sina has some news 2", "Sina has some news 3", "Sina has some news 4", "Sina has some news 5", "Sina has some news 6"],
-                        ["New York Times has some news 1", "New York Times has some news 2", "New York Times has some news 3", "New York Times has some news 4", "New York Times has some news 5", "New York Times has some news 6"],
-                        ["Firefox has some news 1", "Firefox has some news 2", "Firefox has some news 3", "Firefox has some news 4", "Firefox has some news 5", "Firefox has some news 6"],
-                        ["Gilt has some news 1", "Gilt has some news 2", "Gilt has some news 3", "Gilt has some news 4", "Gilt has some news 5", "Gilt has some news 6"]]
+                          "sina", "new york time", "gilt"]
   let childImages = ["c1", "c2", "c3", "c4", "c5", "c6"]
   
   var parentIndex: Int = 0
@@ -69,7 +57,6 @@ extension ViewController: ChainPageCollectionViewProtocol {
   func parentCollectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ImageCardCollectionViewCell.self), for: indexPath)
     if let vCell = cell as? ImageCardCollectionViewCell {
-      vCell.textLabel.text = parentDataSource[indexPath.row]
       vCell.backGroundImageView.image = UIImage(named: parentDataSource[indexPath.row])
     }
     return cell
@@ -77,15 +64,13 @@ extension ViewController: ChainPageCollectionViewProtocol {
   
   
   func childCollectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return childDataSouce[parentIndex].count
+    return childImages.count
   }
   
   func childCollectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ImageCardCollectionViewCell.self), for: indexPath)
     if let vCell = cell as? ImageCardCollectionViewCell {
-      vCell.textLabel.text = childDataSouce[parentIndex][indexPath.row]
-      vCell.textLabel.font = UIFont.systemFont(ofSize: 18)
       vCell.backGroundImageView.image = UIImage(named: childImages[indexPath.row])
     }
     return cell
